fa18cc5b1d5809f6e494de68d762521036f15c0f
mikitaro-27 <matt.diak@gmail.com>
2022-05-11 16:46:58 +0100

    Fixes bug, removes redundant packages

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,20 +1,10 @@
-import ReactMarkdown from "react-markdown";
-import remarkBreaks from "remark-breaks";
-
 const Book = ({ book }) => {
-  console.log(book);
   const imgUrl =
     "https://reststop.randomhouse.com/resources/titles/9786073140546";
-
-  console.log(book.jacketquotes);
 
   return (
     <div className="selected-book">
       <div className="author-bio">{book.authorbio}</div>
-      <ReactMarkdown
-        children={book.flapcopy}
-        remarkPlugins={[remarkBreaks]}
-      ></ReactMarkdown>
       <a
         href={`https://www.amazon.co.uk/s?i=stripbooks&rh=p_66%3A$9786073140546`}
         target="_blank"
