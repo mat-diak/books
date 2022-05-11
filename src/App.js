87de95bebe4e8c971abca7fc65b0d3cac4fec6d5
mikitaro-27 <matt.diak@gmail.com>
2022-05-11 10:23:59 +0100

    Sets up react and tools

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <>Hello</>;
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
